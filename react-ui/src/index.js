8a00fa61ddbb3d9578782e4e32f96d514bc50eef
paul-eng <paul.jo.eng@gmail.com>
2022-06-21 16:20:26 -0400

    Challenge setup

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import CssBaseline from '@mui/material/CssBaseline';
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <CssBaseline>
+    <App />
+  </CssBaseline>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
